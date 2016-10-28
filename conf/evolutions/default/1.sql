797b187dcb626abedd801f721e19321cba762c08
Josef <josef.goebel@posteo.de>
2016-10-28 09:15:13 +0200

    add database constraint regarding fuel types

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -5,7 +5,7 @@
 CREATE TABLE caradvert (
   id VARCHAR(36) PRIMARY KEY,
   title LONGVARCHAR NOT NULL,
-  fuel VARCHAR(255) NOT NULL,
+  fuel VARCHAR(255) NOT NULL check(fuel in ('GASOLINE', 'DIESEL')),
   price INT NOT NULL,
   isnew BOOLEAN NOT NULL,
   mileage INT,
